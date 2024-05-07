f128e61876a4fc59f0fc28c7fd0462a42b294fe4
fallen-lord <abdurashidk150@gmail.com>
2024-05-07 14:09:26 +0500

    fixed lambda response

--- lambda_function.py
+++ lambda_function.py
@@ -8,32 +8,6 @@ from kiril_lotin import translate
 
 ADMIN = consts.ADMIN_ID
 
-def add_id(user_id=None, show_ids=False):
-    import sqlite3
-    try:
-        db = sqlite3.connect("kiril_lotin.db")
-        cursor = db.cursor()
-        cursor.execute("SELECT user_id FROM user")
-        users_id = cursor.fetchall()
-        if not show_ids:
-            if not (user_id, ) in users_id:
-                cursor.execute(f"""
-                  INSERT INTO user VALUES(
-                      {user_id}
-                  )
-                  """)
-                db.commit()
-                # print(f"{user_id} mb ga qo'shildi!!!")
-
-    except Exception as e:
-        print(e)
-
-    finally:
-        cursor.close()
-        db.close()
-    if show_ids:
-        return len(users_id)
-
 
 def send_message(chat_id, text):
     """Chatga habar jo'natuvchi funksiya"""
@@ -41,6 +15,7 @@ def send_message(chat_id, text):
     url = consts.SEND_MESSAGE
     post(url,
         data={'chat_id':chat_id, 'text':text})
+
 
 def lambda_handler(event=None, context=None):
     message = json.loads(event['body']).get('message')
@@ -57,7 +32,6 @@ def lambda_handler(event=None, context=None):
         return response
     
     if text == "/start":
-        add_id(user_id)
         send_message(user_id, """
 Menga habar jo'natishingizdan oldin eslatib o'tmoqchiman:
 Agar matningiz kiril yozuvida bo'lsa lotin, 
