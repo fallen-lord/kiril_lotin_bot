885916827e2b5efcd6da6bd1f2a113af74460cca
fallen-lord <abdurashidk150@gmail.com>
2024-05-07 14:06:34 +0500

    fixed lambda response

--- lambda_function.py
+++ lambda_function.py
@@ -44,16 +44,17 @@ def send_message(chat_id, text):
 
 def lambda_handler(event=None, context=None):
     message = json.loads(event['body']).get('message')
-    if message is None:
-        return {
+    response = {
         'statusCode': 200
+        "body": "Hello from Lambda!"
     }
+
+    if message is None:
+        return response
     text = message.get('text')
     user_id = message['chat']['id']
     if text is None:
-        return {
-        'statusCode': 200
-    }
+        return response
     
     if text == "/start":
         add_id(user_id)
@@ -69,6 +70,4 @@ lotin yozuvida bo'lsa kiril yozuviga o'tkazib beraman!!!""")
         for i in translate(text):
             send_message(user_id, i)
 
-    return {
-        'statusCode': 200
-    }
+    return response
